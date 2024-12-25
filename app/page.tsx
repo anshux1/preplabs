7ac234610e8af8a28ac0cc66e078328f160e6c4a
Anshu kumar singh <anshukrsingh11@gmail.com>
2024-12-25 15:11:53 +0530

    base config

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,210 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+const integrations = [
+  [
+    {
+      id: "integration-1",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-1.svg"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      ),
+    },
+    {
+      id: "integration-2",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-2.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-3",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-3.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-4",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-4.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-5",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-5.svg"
+        />
+      ),
+    },
+  ],
+  [
+    {
+      id: "integration-6",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-6.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-7",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-1.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-8",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-2.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-9",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-3.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-10",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-4.svg"
+        />
+      ),
+    },
+  ],
+  [
+    {
+      id: "integration-11",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-5.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-12",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-6.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-13",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-1.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-14",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-2.svg"
+        />
+      ),
+    },
+    {
+      id: "integration-15",
+      icon: (
+        <img
+          alt="Integration"
+          src="https://shadcnblocks.com/images/block/block-3.svg"
+        />
+      ),
+    },
+  ],
+]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function page() {
+  return (
+    <section className="relative overflow-hidden">
+      <div className="absolute inset-0 overflow-hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1400 600"
+          className="min-h-full min-w-full"
+        >
+          <defs>
+            <pattern
+              id="grid"
+              width="24"
+              height="24"
+              patternUnits="userSpaceOnUse"
+            >
+              <path
+                d="M 24 0 L 0 0 0 24"
+                fill="none"
+                stroke="hsl(var(--muted))"
+                strokeWidth="1"
+                strokeOpacity={0.5}
+              />
+            </pattern>
+          </defs>
+          <rect width="1400" height="600" fill="url(#grid)" />
+        </svg>
+      </div>
+      <div className="relative">
+        <div className="absolute left-0 z-10 hidden h-full w-1/2 bg-[linear-gradient(to_right,hsl(var(--background))_85%,transparent_100%)] md:block"></div>
+        <div className="md:-space-x-26 container relative mx-auto my-auto flex flex-col items-start md:flex-row md:items-center">
+          <div className="z-20 -mx-[calc(theme(container.padding))] w-[calc(100%+2*theme(container.padding))] shrink-0 bg-background px-[calc(theme(container.padding))] pt-32 md:w-1/2 md:bg-transparent md:pb-32">
+            <div className="flex flex-col items-start text-left">
+              <div className="max-w-sm">
+                <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
+                  Welcome to Neet.exam
+                </h1>
+                <Button>Primary</Button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col gap-16 pb-8 pt-12 md:py-32">
+              {integrations.map((line, i) => (
+                <div key={i} className="flex gap-x-24 odd:-translate-x-24">
+                  {line.map((integration) => (
+                    <div
+                      key={integration.id}
+                      className="size-24 rounded-xl border border-background bg-background shadow-xl"
+                    >
+                      <div className="h-full w-full bg-muted/20 p-4">
+                        {integration.icon}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </section>
+  )
 }
